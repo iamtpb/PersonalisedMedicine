72473f88b4eebd60c870026decd587fe8f081c4d
Tejas Bhole <bhole.tejas@live.com>
2017-10-27 01:34:42 +0530

    Added files

--- lstm2.py
+++ lstm2.py
@@ -3,24 +3,8 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import keras
 import numpy as np
-from sklearn.model_selection import cross_val_predict
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import log_loss, accuracy_score
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import SVC
-from sklearn.decomposition import TruncatedSVD
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
-
-import gensim
-
 import scikitplot.plotters as skplt
-
 from sklearn.model_selection import train_test_split
-
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
@@ -30,28 +14,29 @@ from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
 from keras.optimizers import Adam
 
-train = pd.read_csv('data/training_variants')
-test = pd.read_csv('data/stage2_test_variants.csv')
-trainx = pd.read_csv('data/training_text', sep="\|\|", engine='python', header=None, skiprows=1, names=["ID","Text"])
-testx = pd.read_csv('data/stage2_test_text.csv', sep="\|\|", engine='python', header=None, skiprows=1, names=["ID","Text"])
-train = pd.merge(train, trainx, how='left', on='ID').fillna('')
-train = train.drop(["ID"], axis=1)
+# train = pd.read_csv('data/training_variants')
+# test = pd.read_csv('data/stage2_test_variants.csv')
+# trainx = pd.read_csv('data/training_text', sep="\|\|", engine='python', header=None, skiprows=1, names=["ID","Text"])
+# testx = pd.read_csv('data/stage2_test_text.csv', sep="\|\|", engine='python', header=None, skiprows=1, names=["ID","Text"])
+# train = pd.merge(train, trainx, how='left', on='ID').fillna('')
+# train = train.drop(["ID"], axis=1)
 
-test = pd.merge(test, testx, how='left', on='ID').fillna('')
-pid = test['ID'].values
-df_variants_test = pd.read_csv('data/test_variants', usecols=['ID', 'Gene', 'Variation'])
-df_text_test = pd.read_csv('data/test_text', sep='\|\|', engine='python',
-                           skiprows=1, names=['ID', 'Text'])
-df_variants_test['Text'] = df_text_test['Text']
-df_test = df_variants_test
-df_labels_test = pd.read_csv('data/stage1_solution_filtered.csv')
-df_labels_test['Class'] = pd.to_numeric(df_labels_test.drop('ID', axis=1).idxmax(axis=1).str[5:])
-df_test = df_test.merge(df_labels_test[['ID', 'Class']], on='ID', how='left').drop('ID', axis=1)
-df_test = df_test[df_test['Class'].notnull()]
-df_stage_2_train = pd.concat([train, df_test])
-df_stage_2_train.reset_index(drop=True, inplace=True)
-train = df_stage_2_train
-
+# test = pd.merge(test, testx, how='left', on='ID').fillna('')
+# pid = test['ID'].values
+# df_variants_test = pd.read_csv('data/test_variants', usecols=['ID', 'Gene', 'Variation'])
+# df_text_test = pd.read_csv('data/test_text', sep='\|\|', engine='python',
+#                            skiprows=1, names=['ID', 'Text'])
+# df_variants_test['Text'] = df_text_test['Text']
+# df_test = df_variants_test
+# df_labels_test = pd.read_csv('data/stage1_solution_filtered.csv')
+# df_labels_test['Class'] = pd.to_numeric(df_labels_test.drop('ID', axis=1).idxmax(axis=1).str[5:])
+# df_test = df_test.merge(df_labels_test[['ID', 'Class']], on='ID', how='left').drop('ID', axis=1)
+# df_test = df_test[df_test['Class'].notnull()]
+# df_stage_2_train = pd.concat([train, df_test])
+# df_stage_2_train.reset_index(drop=True, inplace=True)
+# train = df_stage_2_train
+train = pd.read_pickle("df_train")
+test = pd.read_pickle("df_test")
 
 num_words = 2000
 tokenizer = Tokenizer(num_words=num_words)
@@ -62,11 +47,22 @@ X = pad_sequences(X, maxlen=2000)
 
 embed_dim = 128
 
-ckpt_callback = ModelCheckpoint('lstm_model_1',
+ckpt_callback = ModelCheckpoint('lstm_model_x',
                                  monitor='val_loss',
                                  verbose=1,
                                  save_best_only=True,
                                  mode='auto')
+
+"""
+model = Sequential()
+model.add(Embedding(num_words, embed_dim, input_length = X.shape[1]))
+model.add(LSTM(196, recurrent_dropout=0.5, dropout=0.5))
+model.add(LSTM(128, recurrent_dropout=0.5, dropout=0.5))
+model.add(LSTM(128, recurrent_dropout=0.5, dropout=0.5))
+model.add(LSTM(64, dropout=0.5))
+model.add(LSTM(64, dropout=0.5))
+model.add(Dense(9,activation='softmax'))
+"""
 
 model = Sequential()
 model.add(Embedding(num_words, embed_dim, input_length = X.shape[1]))
@@ -80,19 +76,21 @@ Y = pd.get_dummies(train['Class']).values
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42, stratify=Y)
 print(X_train.shape, Y_train.shape)
 print(X_test.shape, Y_test.shape)
+
+
 batch_size = 64
 model.fit(X_train, Y_train, epochs=10, batch_size=batch_size, validation_split=0.2, callbacks=[ckpt_callback])
 
-model = load_model('lstm_model_1')
+model = load_model('lstm_model_x')
 
 probas = model.predict(X_test)
 pred_indices = np.argmax(probas, axis=1)
 classes = np.array(range(1, 10))
 preds = classes[pred_indices]
+
 print('Log loss: {}'.format(log_loss(classes[np.argmax(Y_test, axis=1)], probas)))
 print('Accuracy: {}'.format(accuracy_score(classes[np.argmax(Y_test, axis=1)], preds)))
 skplt.plot_confusion_matrix(classes[np.argmax(Y_test, axis=1)], preds)
-
 
 Xtest = tokenizer.texts_to_sequences(testx['Text'].values)
 Xtest = pad_sequences(Xtest, maxlen=2000)
@@ -103,4 +101,4 @@ submission_df = pd.DataFrame(probas, columns=['class'+str(c+1) for c in range(9)
 submission_df['ID'] = df_test['ID']
 submission_df.head()
 
-#submission_df.to_csv('submission1.csv', index=False)
+#submission_df.to_csv('submissionX.csv', index=False)
